1ce3df0b3311754373bfe1c31084a91a897e321b
yingzhang1 <yingzhang1@fintopia.tech>
2023-09-26 18:57:10 +0800

    更新初始化脚本；优化提示

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ prefix=${prefix:-aup}
 
 echo "\033[32m你设置的快捷指令为:$prefix\033[0m"
 echo "生成的alias为:"
-echo "\033[34malias $prefix=\"python $current_abs_path/commitPrefix.py \$1\"\033[0m"
+echo "python使用: \033[34malias $prefix=\"python $current_abs_path/commitPrefix.py \$1\"\033[0m"
+echo "python3使用: \033[34malias $prefix=\"python3 $current_abs_path/commitPrefix.py \$1\"\033[0m"
 echo "我不知道你用的什么sh工具....."
 echo "如果你使用zsh:"
 echo "\033[32m1. vi \$HOME/.zshrc\033[0m"
@@ -16,16 +17,6 @@ echo "如果你使用bash:"
 echo "\033[32m1. vim \$HOME/.bash_rc\033[0m"
 echo "\033[32m2. 把\033[34m上面的蓝色文字\033[0m粘贴到文件中, 然后:wq保存\033[0m"
 echo "\033[32m3. source \$HOME/.bash_rc\033[0m\n"
-
-cat <<EOF > prefixConfig.json
-{
-  "module文件名": "提交的prefix",
-  "whiteList": [
-  ],
-  "multiplePrefix": false
-}
-EOF
-echo "已生成prefixConfig.json文件: \033[32m$current_abs_path/prefixConfig.json\033[0m 记得去配置\n"
 
 echo "使用:"
 echo "\033[32m$prefix '我是提交的内容'\033[0m"
